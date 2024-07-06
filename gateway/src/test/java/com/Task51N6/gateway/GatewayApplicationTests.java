c8252a253128ebf8120783c60ad4744d9ff78ae0
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-07-07 01:12:58 +0300

    First

---
+++ gateway/src/test/java/com/Task51N6/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Task51N6.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
