a314045b8074c64b5c4e377bef63b4eaf3b143e5
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-07-07 13:27:57 +0300

    First

--- location/src/main/java/com/Task51N6/location/LocationApplication.java
+++ location/src/main/java/com/Task51N6/location/LocationApplication.java
@@ -2,8 +2,10 @@ package com.Task51N6.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LocationApplication {
 
 	public static void main(String[] args) {
